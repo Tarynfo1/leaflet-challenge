e090e9561b3dd8ed950f334b745cf60275167091
Taryn Fordyce <fordycetaryn@gmail.com>
2023-09-17 19:53:57 +1000

    added completed logic and css

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # leaflet-challenge
+## Background
+The United States Geological Survey, or USGS for short, is responsible for providing scientific data about natural hazards, the health of our ecosystems and environment, and the impacts of climate and land-use change. Their scientists develop new methods and tools to supply timely, relevant, and useful information about the Earth and its processes.
+
+The USGS is interested in building a new set of tools that will allow them to visualise their earthquake data. They collect a massive amount of data from all over the world each day, but they lack a meaningful way of displaying it. In this challenge, you have been tasked with developing a way to visualise USGS data that will allow them to better educate the public and other government organisations (and hopefully secure more funding) on issues facing our planet.
+
+## Contents
+
+
+### - Step 1. Retrieve dataset:
+
+The USGS provides earthquake data in a number of different formats, updated every 5 minutes. Visit the USGS GeoJSON FeedLinks to an external site. page and choose a dataset to visualise.
+When you click a dataset (such as "All Earthquakes from the Past 7 Days"), you will be given a JSON representation of that data. Use the URL of this JSON to pull in the data for the visualisation
+
+### - Step 2 Import and visualise the data by doing the following:
+
+Using Leaflet, create a map that plots all the earthquakes from a dataset based on longitude and latitude.
+
+Data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by colour. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in colour.
+
+## Acknowledgements
+***
+The following resource: "https://plotly.com/javascript/getting-started/" assisted in creating the below code snippet
+```
+
+```
+
+The following resource: "https://developers.arcgis.com/javascript/latest/api-reference/esri-layers-GeoJSONLayer.html" assisted in creating the below code snippet
+
+```
+
+```<br>
+The following resource: "https://plotly.com/javascript/getting-started/" assisted in creating the below code snippet
+```
+
+```
+The following resource: "https://plotly.com/javascript/getting-started/" assisted in creating the below code snippet
+```
+
+```
---
+++ static/js/logic.js
@@ -0,0 +1,155 @@
+// Define the url for the GeoJSON earthquake data
+
+// console.log("logic.js")
+// var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
+
+// // Create the map parameters
+// var myMap = L.map("map", {
+//     center: [
+//         40.7, -94.5],
+//     zoom: 3   
+// });
+
+// // Add a tile layer to the map
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(myMap);
+console.log("testlogic2.js")
+//Define a baseMaps object to hold the base
+var theMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+let myMap = L.map("map", {
+    center: [
+        40.7, -94.5
+    ],
+    zoom: 3
+});
+
+theMap.addTo(myMap);
+
+let baseMaps = {
+    "Light Global": theMap,
+    "Topo": topo
+};
+
+let tectonicplates = new L.LayerGroup();
+let earthquakes = new L.LayerGroup();
+
+let overlays = {
+    "Tectonic Plates": tectonicplates,
+    "Earthquakes": earthquakes
+};
+
+L
+    .control
+    .layers(baseMaps, overlays, { collapsed: false })
+    .addTo(myMap);
+
+
+// Retrieve and add the earthquake data to the map
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+    function mapStyle(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: mapColor(feature.geometry.coordinates[2]),
+            color: "black",
+            radius: mapRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+
+    // Establish colors to show depth of magnitude
+    }
+    function mapColor(depth) {
+        switch (true) {
+            case depth > 90:
+                return "#5f4e8a";
+            case depth > 70:
+                return "#865d8d";
+            case depth > 50:
+                return "#bb708d";
+            case depth > 30:
+                return "#d0868f";
+            case depth > 10:
+                return "#df9b91";
+            case depth > -10:
+                    return "#F7CD9F";
+            default:
+                return "#3776a1";
+        }
+    }
+    // Establish magnitude size
+    function mapRadius(mag) {
+        if (mag === 0) {
+            return 1;
+        }
+
+        return mag * 6;
+    }
+
+    // Add earthquake data to the map
+    L.geoJson(data, {
+
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+
+        style: mapStyle,
+
+        // Activate pop-up data when circles are clicked
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(`Magnitude: <b>${feature.properties.mag}</b><br>
+                Depth: <b>${feature.geometry.coordinates[2]} km</b><br>
+                Location: <b>${feature.properties.place} </b><br>
+                When: <b>${feature.properties.time/1000}</b>`)
+
+        }
+    }).addTo(myMap);
+
+    let tectonicplates = new L.LayerGroup();
+    let overlays = {
+        "Tectonic Plates": tectonicplates
+    };
+    L.control    
+    d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (platedata) {
+        // Adding our geoJSON data, along with style information, to the tectonicplates
+        // layer.
+        L.geoJson(platedata, {
+          color: "orange",
+          weight: 2
+        })
+          .addTo(tectonicplates);
+    
+        // Then add the tectonicplates layer to the map.
+        tectonicplates.addTo(myMap);
+    });
+
+// Add the legend with colors to corrolate with depth
+
+// Add the legend with colors to correlate with depth
+var legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function () {
+    var div = L.DomUtil.create("div", "info legend");
+    var depthRanges = [-10, 10, 30, 50, 70, 90];
+
+    for (let i = 0; i < depthRanges.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + mapColor(depthRanges[i] + 1) + '"></i> ' +
+            depthRanges[i] + (depthRanges[i + 1] ? '&ndash;' + depthRanges[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
+legend.addTo(myMap);
+
+
+});
